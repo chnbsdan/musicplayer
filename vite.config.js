dc83feddc3538cc2f0d77685b41b2550db49f886
chnbsdan <chnbsdan@gmail.com>
2026-07-19 22:30:49 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/musicplayer/',  // ← 加这行，仓库名是什么就写什么
   server: {
     port: 3000,
     host: true
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
