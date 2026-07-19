8b2efcc193f57a10d264716e9753089c7510fe4b
chnbsdan <chnbsdan@gmail.com>
2026-07-19 09:16:09 +0800

    Create index.js

---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+export const API_CONFIG = {
+  METING_API: 'https://api.injahow.cn/meting/',
+  LRC_API: 'https://api.uomg.com/api/163/lyric',
+  TWIKOO_ENV_ID: 'https://twikoo.hangdn.com',
+  TWIKOO_REGION: 'ap-guangzhou'
+}
+
+export const LYRICS_CONFIG = {
+  updateInterval: 200,
+  minWidth: 150,
+  minHeight: 70,
+  defaultColor: '#ff4500',
+  defaultFontSize: 30
+}
+
+export const PLAYER_CONFIG = {
+  theme: '#49b1f5',
+  preload: 'metadata'
+}
