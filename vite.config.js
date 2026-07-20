3955f8d9e1e31e74e5d48f5d29dcb7340b6198da
chnbsdan <chnbsdan@gmail.com>
2026-07-20 20:46:51 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',  // ← 改成 '/'，不要带仓库名
+  base: './',  // ← 相对路径，适配所有子路径部署
   server: {
     port: 3000,
     host: true
